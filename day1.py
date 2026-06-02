6aa0975cbfab295eddee742972a47f76485b7ec4
Jan Wayne <janwhane9@gmail.com>
2026-06-02 20:52:19 +0800

    day1

---
+++ day1.py
@@ -0,0 +1,14 @@
+name = "Wayne"
+age = 20
+course = "Computer Science"
+dream_job = "Software Engineer"
+
+print(f"This is name: {name}\n{age}\n{course}\n{dream_job}\n")
+
+
+# how to do new line without printing like this:
+
+print(name)
+print(age)
+print(course)
+print(dream_job)
