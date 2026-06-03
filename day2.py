c3f2b0bf5b811890639a9f7c962b8923d6ae7c84
Jan Wayne <janwhane9@gmail.com>
2026-06-03 14:21:54 +0800

    day 2 of learning

---
+++ day2.py
@@ -0,0 +1,8 @@
+name = input("Name: ")
+age = input("Age: ")
+course = input("Course: ")
+dream_job = input("Dream Job: ")
+favorite_language = input("Favorite programming language: ")
+
+
+print(f"Welcome {name}!\nLet's create your student profile\n====STUDENT PROFILE===\nName: {name}\nAge: {age}\nCourse: {course}\nDream Job: {dream_job}\nFavorite programming language: {favorite_language}")
